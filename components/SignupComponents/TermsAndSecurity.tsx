4f5e9915ca3f06e9257713382ea4efdca729b3a4
Denis Tibah <denis.tibah0218@gmail.com>
2023-07-20 20:38:43 +0800

    add terms and security section

--- components/SignupComponents/TermsAndSecurity.tsx
+++ components/SignupComponents/TermsAndSecurity.tsx
@@ -1,8 +1,17 @@
-import { FC } from "react";
+import { FC, useState, Fragment } from "react";
 import { View, Text } from "react-native";
+import { useFormik } from "formik";
+import { useSelector, useDispatch } from "react-redux";
 
 import Typography from "../../components/Typography";
+import FixedBottomAction from "../../components/FixedBottomAction";
+import ButtonSubmit from "../../components/Button";
+import ArrowLeft from "../../assets/icons/ArrowLeft";
+import ArrowRightLong from "../../assets/icons/ArrowRightLong";
 import { Seperator } from "../../components/Seperator/Seperator";
+import { termsAndConditionSchema } from "../../utils/formikSchema";
+import { setRegistrationData } from "../../redux/registration/registrationSlice";
+import FormGroup from "../../components/FormGroup";
 import vars from "../../styles/vars";
 import { styles } from "./styles";
 
@@ -11,10 +20,45 @@ interface ITermsAndSecurity {
   handleNextStep: () => void;
 }
 
+const PRIVACY_SETTINGS = [
+  { label: "I've read and accept the Terms", value: "termsAndConditions" },
+  {
+    label: "I've read and accept the Provicy Policy",
+    value: "readPrivacyPolicy",
+  },
+];
 const TermsAndSecurity: FC<ITermsAndSecurity> = ({
   handlePrevStep,
   handleNextStep,
 }) => {
+  const dispatch = useDispatch();
+  const registration = useSelector((state: any) => state.registration);
+
+  const { handleSubmit, handleChange, values, touched, errors, setFieldValue } =
+    useFormik({
+      validationSchema: termsAndConditionSchema,
+      initialValues: {
+        termsAndConditions: registration?.data?.termsAndConditions || false,
+        readPrivacyPolicy: registration?.data?.readPrivacyPolicy || false,
+        newsLetterSubscription:
+          registration?.data?.newsLetterSuncription || false,
+      },
+      onSubmit: ({
+        termsAndConditions,
+        readPrivacyPolicy,
+        newsLetterSubscription,
+      }) => {
+        dispatch(
+          setRegistrationData({
+            termsAndConditions,
+            readPrivacyPolicy,
+            newsLetterSuncription: newsLetterSubscription,
+          })
+        );
+        handleNextStep();
+      },
+    });
+
   return (
     <View style={styles.card}>
       <View style={styles.cardTitle}>
@@ -23,6 +67,95 @@ const TermsAndSecurity: FC<ITermsAndSecurity> = ({
         </Typography>
       </View>
       <Seperator backgroundColor={vars["grey"]} marginBottom={24} />
+      <View>
+        <View style={styles.cardBody}>
+          <View>
+            <Text style={styles.termsHeaderText}>Privacy settings</Text>
+            <View>
+              {PRIVACY_SETTINGS.map(
+                ({ label, value }: { label: String; value: string }) => {
+                  return (
+                    <Fragment key={value}>
+                      <FormGroup
+                        validationError={
+                          errors[value as keyof typeof values] &&
+                          touched[value as keyof typeof values] &&
+                          errors[value as keyof typeof values]
+                        }
+                      >
+                        <FormGroup.CheckboxUI
+                          label={label}
+                          value={values[value as keyof typeof values]}
+                          color={
+                            values[value as keyof typeof values]
+                              ? vars["accent-pink"]
+                              : undefined
+                          }
+                          onValueChange={() => {
+                            setFieldValue(
+                              value,
+                              !values[value as keyof typeof values]
+                            );
+                          }}
+                        />
+                      </FormGroup>
+                    </Fragment>
+                  );
+                }
+              )}
+            </View>
+          </View>
+          <View>
+            <Text style={styles.termsHeaderText}>Optional</Text>
+            <View>
+              <FormGroup>
+                <FormGroup.CheckboxUI
+                  label="Newsletter subscription"
+                  value={values?.newsLetterSubscription}
+                  color={
+                    values?.newsLetterSubscription
+                      ? vars["accent-pink"]
+                      : undefined
+                  }
+                  onValueChange={() => {
+                    setFieldValue(
+                      "newsLetterSubscription",
+                      !values?.newsLetterSubscription
+                    );
+                  }}
+                />
+              </FormGroup>
+            </View>
+          </View>
+          <FixedBottomAction rounded>
+            <View
+              style={{
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                paddingRight: 20,
+              }}
+            >
+              <ButtonSubmit
+                color="light-pink"
+                onPress={handlePrevStep}
+                leftIcon={<ArrowLeft size={14} />}
+              >
+                Back
+              </ButtonSubmit>
+              <ButtonSubmit
+                color="light-pink"
+                onPress={handleSubmit}
+                leftIcon={<ArrowRightLong size={14} />}
+              >
+                Continue
+              </ButtonSubmit>
+            </View>
+          </FixedBottomAction>
+        </View>
+      </View>
     </View>
   );
 };

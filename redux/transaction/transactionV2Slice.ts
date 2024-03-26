f1940af0543f4c8a8082cbae87292880c98bdb6b
Denis Tibah <denis.tibah0218@gmail.com>
2024-03-26 12:37:39 +0200

    added new merge transactionV2 webservice to redux

--- redux/transaction/transactionV2Slice.ts
+++ redux/transaction/transactionV2Slice.ts
@@ -18,7 +18,7 @@ export const transactionV2 = createApi({
       query: (params: any) => {
         const { isGroupingDisabled } = params;
         return {
-          url: `/transactions`,
+          url: `/transactionsV2`,
           method: "POST",
           body: {
             account_id: params?.accountId,
